eace88b0dfd3163cfead4bf713d8be6f1cef631d
Iikka <t8kuii00@students.oamk.fi>
2021-11-08 11:52:08 +0200

    copied the part1 courseinfo

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part title={parts[0].name} count={parts[0].exercises} />
+      <Part title={parts[1].name} count={parts[1].exercises} />
+      <Part title={parts[2].name} count={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = ({ title, count }) => {
+  return (
+    <div>
+      <p>{title} {count}</p>
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div className="App">
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+}
+
+export default App;
