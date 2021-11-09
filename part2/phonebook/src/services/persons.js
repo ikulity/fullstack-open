d3895a9709942e3f5db9ca1770eb40567f284194
Iikka <t8kuii00@students.oamk.fi>
2021-11-09 20:36:15 +0200

    Exercise 3.9

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 // backend communication implemented already!
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
